157eaf1a03ea1b67e45d79bba63d67c3e83de843
Mauricio <mauricio.o.mesquita@gmail.com>
2025-01-22 19:42:05 -0500

    PR-15.5 uncomment has property

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@
 const nextConfig = {
   async rewrites() {
     return [
-      // These rewrites are checked after headers/redirects
-      // and before all files including _next/public files which
-      // allows overriding page files
       {
         source: "/sobre",
         destination: "/login",
-        // has: [{ type: "host", key: "overrideMe", value: "wp.*" }],
+        has: [{ type: "host", key: "overrideMe", value: "wp.*" }],
       },
     ];
   },
