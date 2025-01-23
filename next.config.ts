8060e0018857e7f42a1e53b28c028ac3a3cd7967
Mauricio <mauricio.o.mesquita@gmail.com>
2025-01-22 19:38:10 -0500

    PR-15.4 another test using valid pages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
+  async rewrites() {
     return [
       // These rewrites are checked after headers/redirects
       // and before all files including _next/public files which
       // allows overriding page files
       {
-        source: "/:path",
-        destination: "/api/wp/:path",
-        has: [{ type: "host", key: "overrideMe", value: "wp.*" }],
-        permanent: true,
+        source: "/sobre",
+        destination: "/login",
+        // has: [{ type: "host", key: "overrideMe", value: "wp.*" }],
       },
     ];
   },
