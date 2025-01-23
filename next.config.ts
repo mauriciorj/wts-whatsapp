6e11641a2371147e0b6a1042f24b1d8818438275
Mauricio <mauricio.o.mesquita@gmail.com>
2025-01-22 22:45:34 -0500

    PR-17.2 remove redirect

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://zaprouter.pro/wp/:path*",
-        has: [
-          {
-            type: "host",
-            // key: "x-forwarded-host",
-            value: "(?<wp>.+)",
-          },
-        ],
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
